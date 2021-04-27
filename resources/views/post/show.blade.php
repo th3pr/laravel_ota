114651a8211da9f49f1b929a36a813cc53bd9350
Mohamed A. Bahnsawy <vipme1@outlook.com>
2021-04-27 04:29:11 +0200

    Fix FileManager

--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -12,41 +12,41 @@
             <div class="card mb-5">
                 <div class="card-body">
                     <div class="row">
-                        <div class="col-sm-1">
+                        <div class="col-sm-4">
                             Title
                         </div>
-                        <div class="col-sm-3">
+                        <div class="col-sm-4">
                             <strong>{{ $post->post_title }}</strong>
                         </div>
                         <div class="col-sm-4">
                             @if ($post->featured_image)
-                                <a href="{{ asset('storage/'.$post->featured_image) }}" target="_blank">
-                                    <img width="150" height="150" class="img-fluid" src="{{ asset('storage/'.$post->featured_image) }}" alt="">
+                                <a href="{{ asset($post->featured_image) }}" target="_blank">
+                                    <img width="250" height="250" class="img-fluid" src="{{ asset($post->featured_image) }}" alt="">
                                 </a>
                             @endif
                         </div>
                     </div>
                     <div class="row">
-                        <div class="col-sm-1">
+                        <div class="col-sm-4">
                             Category
                         </div>
-                        <div class="col-sm-3">
+                        <div class="col-sm-4">
                             <strong>{{ $post->category->category_name }}</strong>
                         </div>
                     </div>
                     <div class="row">
-                        <div class="col-sm-1">
+                        <div class="col-sm-4">
                             Created By
                         </div>
-                        <div class="col-sm-3">
+                        <div class="col-sm-4">
                             <strong>{{ $post->user->name }}</strong>
                         </div>
                     </div>
                     <div class="row">
-                        <div class="col-sm-1">
+                        <div class="col-sm-4">
                             Body
                         </div>
-                        <div class="col-sm-3">
+                        <div class="col-sm-4">
                             <strong>{!! $post->post_body !!}</strong>
                         </div>
                     </div>
