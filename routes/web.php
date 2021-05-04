f31956aa1357e630c6d5170b4382c8a770953233
Ahmed Ibrahim <ahmedibrahim2621998@gmail.com>
2021-05-04 22:10:39 +0200

    some New Changes

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ Route::get('/', function () {
     return redirect()->route('home');
 });
 Route::get('/home', 'HomeController@index')->name('home');
-Route::get('/searchHotel', 'searchController@indexHotel')->name('searchHotel');
+//Route::get('/searchHotel', 'searchController@indexHotel')->name('searchHotel');
 Route::get('/searchTour', 'searchController@indexTour')->name('searchTour');
 Route::get('/searchCar', 'searchController@indexcar')->name('searchCar');
 
